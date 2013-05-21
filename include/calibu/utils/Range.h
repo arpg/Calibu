773235ba4ff05de9625c399ff10678dcaf9bfc5c
Steven Lovegrove <stevenlovegrove@gmail.com>
2013-05-21 16:08:00 -0400

    Rectify: Adjust new intrinsics to avoid dead image area.

---
+++ include/calibu/utils/Range.h
@@ -0,0 +1,102 @@
+/* 
+   This file is part of the Calibu Project.
+   https://robotics.gwu.edu/git/calibu
+   
+   Copyright (C) 2013 George Washington University,
+                      Steven Lovegrove
+                      
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+   
+   http://www.apache.org/licenses/LICENSE-2.0
+   
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+#pragma once
+
+namespace calibu
+{
+
+/// Represet min/max range.
+struct Range
+{
+    static Range Open()
+    {
+        return Range(-std::numeric_limits<double>::max(),
+                     std::numeric_limits<double>::max());
+    }
+
+    static Range Closed()
+    {
+        return Range(std::numeric_limits<double>::max(),
+                     -std::numeric_limits<double>::max());
+    }
+    
+    inline Range()
+        : minr(std::numeric_limits<double>::max()),
+          maxr(-std::numeric_limits<double>::max())
+    {
+        // Empty range
+    }
+    
+    inline Range(double minr, double maxr)
+        : minr(minr), maxr(maxr)
+    {
+    }
+    
+    inline bool Empty() const
+    {
+        return maxr <= minr;
+    }
+    
+    // Expand range to include v
+    inline void Insert(double v)
+    {
+        minr = std::min(minr,v);
+        maxr = std::max(maxr,v);
+    }
+    
+    // Enforce that range is greater than v
+    inline void ExcludeLessThan(double v)
+    {
+        minr = std::max(minr,v);
+    }
+
+    // Enforce that range is less than v
+    inline void ExcludeGreaterThan(double v)
+    {
+        maxr = std::min(maxr,v);
+    }
+    
+    inline double Size() const
+    {
+        return maxr - minr;
+    }
+    
+    double minr;
+    double maxr;
+};
+
+inline Range Union(const Range& lhs, const Range& rhs)
+{
+    return Range( std::min(lhs.minr, rhs.minr), std::max(lhs.maxr, rhs.maxr) );
+}
+
+inline Range Intersection(const Range& lhs, const Range& rhs)
+{
+    return Range( std::max(lhs.minr, rhs.minr), std::min(lhs.maxr, rhs.maxr) );
+}
+
+std::ostream& operator<<(std::ostream& os, const Range& r)
+{
+    os << "[" << r.minr << ", " << r.maxr << "]";
+    return os;
+}
+
+}
--- src/cam/StereoRectify.cpp
+++ src/cam/StereoRectify.cpp
@@ -19,6 +19,7 @@
  */
 
 #include <calibu/cam/StereoRectify.h>
+#include <calibu/utils/Range.h>
 
 namespace calibu
 {
@@ -38,10 +39,40 @@ void CreateLookupTable(
             // Clamp to valid image coords
             p_warped[0] = std::min(std::max(0.0, p_warped[0]), cam_from.Width() - 1.0 );
             p_warped[1] = std::min(std::max(0.0, p_warped[1]), cam_from.Height() - 1.0 );
+
+//            // Set to (0,0)
+//            if(!(0 < p_warped[0] && p_warped[0] < (cam_from.Width()-1)
+//                 && 0 < p_warped[1] && p_warped[1] < (cam_from.Height()-1))) {
+//                p_warped = Eigen::Vector2d::Zero();
+//            }
             
             lookup_warp(r,c) = p_warped.cast<float>();
         }
     }
+}
+
+Range MinMaxRotatedCol( const calibu::CameraModelInterface& cam, const Eigen::Matrix3d& Rnl_l )
+{
+    Range range = Range::Open();
+    for(size_t row = 0; row < cam.Height(); ++row) {    
+        const Eigen::Vector2d ln = Project(Eigen::Vector3d(Rnl_l*Unproject(cam.Unmap(Eigen::Vector2d(0,row)))));
+        const Eigen::Vector2d rn = Project(Eigen::Vector3d(Rnl_l*Unproject(cam.Unmap(Eigen::Vector2d(cam.Width()-1,row)))));
+        range.ExcludeLessThan(ln[0]);
+        range.ExcludeGreaterThan(rn[0]);
+    }
+    return range;
+}
+
+Range MinMaxRotatedRow( const calibu::CameraModelInterface& cam, const Eigen::Matrix3d& Rnl_l )
+{
+    Range range = Range::Open();
+    for(size_t col = 0; col < cam.Width(); ++col) {    
+        const Eigen::Vector2d tn = Project(Eigen::Vector3d(Rnl_l*cam.UnmapUnproject(Eigen::Vector2d(col,0)) ));
+        const Eigen::Vector2d bn = Project(Eigen::Vector3d(Rnl_l*cam.UnmapUnproject(Eigen::Vector2d(col,cam.Height()-1)) ));
+        range.ExcludeLessThan(tn[1]);
+        range.ExcludeGreaterThan(bn[1]);
+    }
+    return range;
 }
 
 calibu::CameraModelT<Pinhole> CreateScanlineRectifiedLookupAndCameras(
@@ -75,21 +106,39 @@ calibu::CameraModelT<Pinhole> CreateScanlineRectifiedLookupAndCameras(
     const Eigen::Vector3d y_l = z_l.cross(x_l).normalized();
     
     // New orientation for both left and right cameras (expressed relative to original left)
-    Eigen::Matrix3d mRl_nl;
-    mRl_nl << x_l, y_l, z_l;
+    Eigen::Matrix3d Rnl_l;
+    Rnl_l << x_l, y_l, z_l;
     
     // By definition, the right camera now lies exactly on the x-axis with the same orientation
     // as the left camera.
     T_nr_nl = Sophus::SE3d(Eigen::Matrix3d::Identity(), Eigen::Vector3d(-r_l.norm(),0,0) );
 
+    // Work out parameters of new linear camera
+    const Range range_width = //Intersection(
+                MinMaxRotatedCol(cam_left, Rnl_l);
+//                MinMaxRotatedCol(cam_right, Rnl_l)
+//                );
+    const Range range_height = //Intersection(
+                MinMaxRotatedRow(cam_left, Rnl_l);
+//                MinMaxRotatedRow(cam_right, Rnl_l)
+//                );
+
+    // We want to map range width/height to image via K.
+    std::cout << range_width << std::endl;
+    std::cout << range_height << std::endl;
+    
+    const double fu = (cam_left.Width()-1) / range_width.Size();
+    const double fv = (cam_left.Height()-1) / range_height.Size();
+    const double u0 = -fu * range_width.minr;
+    const double v0 = -fv * range_height.minr;
+    
     // Setup new camera
-    const Eigen::Matrix3d Kl = cam_left.K();
     calibu::CameraModelT<Pinhole> new_cam(cam_left.Width(),cam_left.Height());
-    new_cam.Params() << Kl(0,0), Kl(1,1), Kl(0,2), Kl(1,2);    
+    new_cam.Params() << fu, fv, u0, v0;    
     
     // Homographies which should be applied to left and right images to scan-line rectify them
-    const Eigen::Matrix3d Rl_nlKlinv = mRl_nl.transpose() * new_cam.Kinv();
-    const Eigen::Matrix3d Rr_nrKlinv = (mRl_nl * R_lr.matrix()).transpose() * new_cam.Kinv();
+    const Eigen::Matrix3d Rl_nlKlinv = Rnl_l.transpose() * new_cam.Kinv();
+    const Eigen::Matrix3d Rr_nrKlinv = R_lr.inverse().matrix() * Rnl_l.transpose() * new_cam.Kinv();
     
     CreateLookupTable(cam_left, Rl_nlKlinv, dlookup_left);
     CreateLookupTable(cam_right, Rr_nrKlinv, dlookup_right);
