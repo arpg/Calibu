5c5494d664cfbfe6840c8c0453cc11776a9d1340
Gabe Sibley <gsibley@gwu.edu>
2013-06-02 15:35:00 -0400

    Fixed RoboticsRdf matrix.

--- include/calibu/cam/CameraRig.h
+++ include/calibu/cam/CameraRig.h
@@ -61,7 +61,7 @@ static const Sophus::SO3d RdfVision =
 
 static const Sophus::SO3d RdfRobotics = 
         Sophus::SO3d( (Eigen::Matrix3d() << 0,1,0, 0,0,1, 1,0,0).finished() );
-//        Sophus::SO3d( (Eigen::Matrix3d() << 0,0,1, 1,0,0, 0,1,0).finished() );  <-- no: RDF = [x';y';z'] x = forward, y = right, z = down. GTS
+//        Sophus::SO3d( (Eigen::Matrix3d() << 0,0,1, 1,0,0, 0,1,0).finished() );  <-- no: RDF = [right';down';forward']. GTS
 
 // T_2b_1b = T_ba * T_2a_1a * T_ab
 inline Sophus::SE3d ToCoordinateConvention(
