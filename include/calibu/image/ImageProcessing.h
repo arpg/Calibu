490fe569fa1aaef0bba9834a8d7a671ca6acaf96
Jack Morrison <jackmorrison1@gmail.com>
2013-11-14 13:44:58 -0800

    Fixes crashes in ImageProcessing.

--- include/calibu/image/ImageProcessing.h
+++ include/calibu/image/ImageProcessing.h
@@ -1,80 +1,71 @@
-/* 
-   This file is part of the Calibu Project.
-   https://github.com/gwu-robotics/Calibu
+/*
+  This file is part of the Calibu Project.
+  https://github.com/gwu-robotics/Calibu
 
-   Copyright (C) 2013 George Washington University,
-                      Steven Lovegrove
+  Copyright (C) 2013 George Washington University,
+  Steven Lovegrove
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
 
-   http://www.apache.org/licenses/LICENSE-2.0
+  http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
- */
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
 
 #pragma once
 
 #include <Eigen/Eigen>
 #include "Label.h"
 
-namespace calibu
-{
+namespace calibu {
 
-struct ParamsImageProcessing
-{
-    ParamsImageProcessing() :
-        at_threshold(0.7),
-        at_window_ratio(3),
-        black_on_white(true)
-    {
-        
-    }
-    
-    float at_threshold;
-    int at_window_ratio;
-    bool black_on_white;
+struct ParamsImageProcessing {
+  ParamsImageProcessing() : at_threshold(0.7),
+                            at_window_ratio(3),
+                            black_on_white(true) {}
+  float at_threshold;
+  int at_window_ratio;
+  bool black_on_white;
 };
 
-class ImageProcessing
-{
-public:
-    ImageProcessing(int maxWidth, int maxHeight);
-    ~ImageProcessing();
-    
-    void Process(unsigned char* greyscale_image, size_t w, size_t h, size_t pitch);
-    
-    inline int Width()  const { return width; }
-    inline int Height() const { return height; }
-    
-    inline const unsigned char* Img() const { return I; }    
-    inline const Eigen::Vector2f* ImgDeriv() const { return dI; }
-    inline const unsigned char* ImgThresh() const { return tI; }    
-    inline const std::vector<PixelClass>& Labels() const { return labels; }
-    
-    ParamsImageProcessing& Params() { return params; }
-    
-protected:
-    void AllocateImageData(int maxPixels);
-    void DeallocateImageData();
-    
-    int width, height;
-    
-    // Images owned by this class
-    unsigned char* I;
-    float* intI;
-    Eigen::Vector2f* dI;
-    short* lI;
-    unsigned char* tI;
-    
-    std::vector<PixelClass> labels;
-    ParamsImageProcessing params;
-};
+class ImageProcessing {
+ public:
+  ImageProcessing(int maxWidth, int maxHeight);
+  ~ImageProcessing();
 
+  void Process(unsigned char* greyscale_image, size_t w, size_t h, size_t pitch);
+
+  inline int Width()  const { return width; }
+  inline int Height() const { return height; }
+
+  inline const unsigned char* Img() const { return &I[0]; }
+  inline const Eigen::Vector2f* ImgDeriv() const { return &dI[0]; }
+  inline const unsigned char* ImgThresh() const { return &tI[0]; }
+  inline const std::vector<PixelClass>& Labels() const { return labels; }
+
+  ParamsImageProcessing& Params() { return params; }
+
+ protected:
+  void AllocateImageData(int maxPixels);
+  void DeallocateImageData();
+
+  int width, height;
+
+  // Images owned by this class
+  std::vector<unsigned char> I;
+  std::vector<float> intI;
+  std::vector<Eigen::Vector2f, Eigen::aligned_allocator<Eigen::Vector2f> > dI;
+  std::vector<short> lI;
+  std::vector<unsigned char> tI;
+
+  std::vector<PixelClass> labels;
+  ParamsImageProcessing params;
+};
 
 }
--- src/image/ImageProcessing.cpp
+++ src/image/ImageProcessing.cpp
@@ -1,22 +1,22 @@
-/* 
-   This file is part of the Calibu Project.
-   https://github.com/gwu-robotics/Calibu
+/*
+  This file is part of the Calibu Project.
+  https://github.com/gwu-robotics/Calibu
 
-   Copyright (C) 2013 George Washington University,
-                      Steven Lovegrove
+  Copyright (C) 2013 George Washington University,
+  Steven Lovegrove
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
 
-   http://www.apache.org/licenses/LICENSE-2.0
+  http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
- */
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
 
 #include <calibu/image/ImageProcessing.h>
 #include <calibu/image/Gradient.h>
@@ -24,67 +24,60 @@
 #include <calibu/image/IntegralImage.h>
 #include <calibu/image/Label.h>
 
-namespace calibu
-{
+namespace calibu {
 
 ImageProcessing::ImageProcessing(int maxWidth, int maxHeight)
-    : width(maxWidth), height(maxHeight), I(nullptr)
-{
-    AllocateImageData(maxWidth*maxHeight);
+    : width(maxWidth), height(maxHeight) {
+  AllocateImageData(maxWidth*maxHeight);
 }
 
-ImageProcessing::~ImageProcessing()
-{
-    DeallocateImageData();
+ImageProcessing::~ImageProcessing() {
+  DeallocateImageData();
 }
 
-void ImageProcessing::AllocateImageData(int maxPixels)
-{    
-    I = new unsigned char[maxPixels];
-    intI = new float[maxPixels];
-    dI = new Eigen::Vector2f[maxPixels];
-    lI = new short[maxPixels];
-    tI = new unsigned char[maxPixels];
+void ImageProcessing::AllocateImageData(int maxPixels) {
+  I.resize(maxPixels);
+  intI.resize(maxPixels);
+  dI.resize(maxPixels);
+  lI.resize(maxPixels);
+  tI.resize(maxPixels);
 }
 
-void ImageProcessing::DeallocateImageData()
-{
-    delete[] I;
-    delete[] intI;
-    delete[] dI;
-    delete[] lI;
-    delete[] tI;
-}
+void ImageProcessing::DeallocateImageData() {}
 
-void ImageProcessing::Process(unsigned char* greyscale_image, size_t w, size_t h, size_t pitch)
-{
-    width = w;
-    height = h;
+void ImageProcessing::Process(unsigned char* greyscale_image,
+                              size_t w, size_t h, size_t pitch) {
+  width = w;
+  height = h;
 
-    // Copy input image
-    if(pitch > width*sizeof(unsigned char) ) {
-        // Copy line by line
-        for(int y=0; y < height; ++y) {
-            memcpy(I+y*width, greyscale_image+y*pitch, width * sizeof(unsigned char) );
-        }
-    }else{
-        memcpy(I, greyscale_image, width * height * sizeof(unsigned char) );
+  // Copy input image
+  if(pitch > width*sizeof(unsigned char) ) {
+    // Copy line by line
+    for(int y=0; y < height; ++y) {
+      memcpy(&I[y*width], greyscale_image+y*pitch,
+             width * sizeof(unsigned char) );
     }
-    
-    // Process image
-    gradient<>(width, height, I, dI );
-    integral_image(width, height, I, intI );   
-    
-    // Threshold image
-    AdaptiveThreshold(
-                width, height, I, intI, tI, params.at_threshold,
-                width / params.at_window_ratio, 20,
-                (unsigned char)0, (unsigned char)255
-                );    
-    
-    // Label image (connected components)
-    labels.clear();
-    Label(width,height,tI,lI,labels, params.black_on_white ? 0 : 255 );
+  }else{
+    I.resize(width * height * sizeof(unsigned char));
+    memcpy(&I[0], greyscale_image, width * height * sizeof(unsigned char));
+  }
+
+  // Process image
+  Eigen::Vector2f* di_ptr = &dI[0];
+  gradient<>(width, height, &I[0],  di_ptr);
+  integral_image(width, height, &I[0], &intI[0] );
+
+  // Threshold image
+  AdaptiveThreshold(
+      width, height, &I[0], &intI[0], &tI[0], params.at_threshold,
+      width / params.at_window_ratio, 20,
+      (unsigned char)0, (unsigned char)255
+                    );
+
+  // Label image (connected components)
+  labels.clear();
+  Label(width, height, &tI[0], &lI[0], labels,
+        params.black_on_white ? 0 : 255 );
 }
 
 }
