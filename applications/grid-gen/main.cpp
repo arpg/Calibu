d9f83a4dec2b7e04915a2e881de912bff323a877
Jack Morrison <jackmorrison1@gmail.com>
2014-02-16 21:28:09 -0500

    Comments out missing SaveEPS in grid-gen.

--- applications/grid-gen/main.cpp
+++ applications/grid-gen/main.cpp
@@ -19,25 +19,24 @@ void UserQuit(int)
 }
 
 int main( int, char** )
-{    
+{
     signal(SIGABRT,UserQuit);
     signal(SIGTERM,UserQuit);
-        
+
     const int PR = 10;
     const int PC = 19;
-        
+
     uint32_t seed = FindBestSeed(PR, PC, should_run); // 14
     const std::array<Eigen::MatrixXi,4> PG = MakePatternGroup(PR, PC, seed);
-    
+
     std::cout << PG[0] << std::endl;
     std::cout << "Best seed: " << seed << std::endl;
     std::cout << "Min unique window size: " << AutoCorrelationMinArea(PG) << std::endl;
-    
+
     const int auto_corr = AutoCorrelation(PG,5,5);
     std::cout << "Bad auto correlation matches: " << auto_corr << std::endl;
-    
-    SaveEPS("target.eps", PG[1], Eigen::Vector2d(0,0), 10.0, 2.0, 3.0, 4.0 );
-    
+
+    //SaveEPS("target.eps", PG[0], Eigen::Vector2d(0,0), 10.0, 2.0, 3.0, 4.0 );
+
     return 0;
 }
-
